52e71f444697a07e328b2f1887b091db48a3357f
Vamshikrishna939 <163495327+Vamshikrishna939@users.noreply.github.com>
2025-04-26 23:10:00 +0530

    Add files via upload

---
+++ is_palindrome.py
@@ -0,0 +1,12 @@
+"""def is_palindrome(s):
+    s=s.lower().replace(" ","")
+    if s==s[::-1] :
+        return True
+    else:
+        return False
+
+print(is_palindrome("assa"))
+"""
+def string_reverse(s):
+    return s[::-1]
+print(string_reverse("kacha badam"))
