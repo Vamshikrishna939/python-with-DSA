7db037fb3488c3f331602f0e0c67bd468ea9193b
Vamshikrishna939 <163495327+Vamshikrishna939@users.noreply.github.com>
2025-05-15 12:59:39 +0530

    Add files via upload

---
+++ match statement in python.py
@@ -0,0 +1,18 @@
+a=input("enter the input")
+match a:
+    case "p" :
+            print("sunday")
+    case "q" :
+            print("monday")
+    case "r" :
+            print("tuesday")
+    case "s" :
+            print("wednesday")
+    case "t":
+            print("thursday")
+    case "u":
+            print("friday")
+    case "v":
+            print("saturday")
+    case _:
+            print("please enter the valid input")                                                                
